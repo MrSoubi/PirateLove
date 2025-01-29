889c40c08d3a26d6c7e2160a8e70f917eba44f1f
Emmanuel BOUIS <e_bouis@stu-bellecour.fr>
2025-01-29 18:43:25 +0100

    Cursor fix on door

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -7,3 +7,9 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed:
 			TRAVEL_EVENT.triggered.emit(destination)
+
+func _on_mouse_entered() -> void:
+	CursorManager.set_cursor(CursorManager.Type.DOOR)
+
+func _on_mouse_exited() -> void:
+	CursorManager.set_cursor(CursorManager.Type.NONE)
